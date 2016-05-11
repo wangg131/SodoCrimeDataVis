b4c7fc327c55999cec7af4c33688fb562a3f33e8
Loren Wang <wang131.131@gmail.com>
2016-05-10 17:25:36 -0700

    added the base URI for getting data from api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  SEA_CRIME_URI = "https://data.seattle.gov/resource/pu5n-trf4.json"
+  
 end
