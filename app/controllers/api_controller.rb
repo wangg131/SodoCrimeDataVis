d344b05cc435f8b4280eb2b3fb6ed8b77ca1e0e6
Loren Wang <wang131.131@gmail.com>
2016-05-11 01:30:42 -0700

    get request for data within a mile of the stadium

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,7 @@
+require 'httparty'
+
 class ApiController < ApplicationController
   def home
+    @data = HTTParty.get(SEA_CRIME_URI + "?$where=within_circle(incident_location, 47.5951456, -122.331601, 1609.34)")
   end
 end
