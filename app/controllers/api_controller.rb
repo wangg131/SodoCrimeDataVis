3c7f95dd99419ae7c82c457420aea965852921d6
Loren Wang <wang131.131@gmail.com>
2016-05-16 15:44:20 -0700

    setting up app tokens and potential data for mapbox

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,6 +2,7 @@ require 'httparty'
 
 class ApiController < ApplicationController
   def home
+    @geojson = Array.new
     headers = { "$$app_token" => "#{ENV['APP_TOKEN']}"}
     query   = { "$where" => "within_circle(incident_location, 47.5951456, -122.331601, 1609.34)"}
     @data = HTTParty.get(SEA_CRIME_URI, :headers => headers, :query => query)
