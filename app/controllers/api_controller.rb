a73223a4c6536037ec126185233665d916faa89f
Loren Wang <wang131.131@gmail.com>
2016-05-16 15:56:21 -0700

    respond with either html or json

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,9 +2,12 @@ require 'httparty'
 
 class ApiController < ApplicationController
   def home
-    @geojson = Array.new
     headers = { "$$app_token" => "#{ENV['APP_TOKEN']}"}
     query   = { "$where" => "within_circle(incident_location, 47.5951456, -122.331601, 1609.34)"}
-    @data = HTTParty.get(SEA_CRIME_URI, :headers => headers, :query => query)
+    data = HTTParty.get(SEA_CRIME_URI, :headers => headers, :query => query)
+    respond_to do |format|
+      format.html
+      format.json { render json: data }
+    end
   end
 end
