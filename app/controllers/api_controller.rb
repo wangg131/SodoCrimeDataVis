b8e4d1c6eb2360a81a8b435e1dbfbed0585f71f1
Loren Wang <wang131.131@gmail.com>
2016-05-16 18:24:58 -0700

    wip filtering for top 5 events

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,16 +2,17 @@ require 'httparty'
 
 class ApiController < ApplicationController
   def home
-    begin
+    # begin
       headers = { "$$app_token" => "#{ENV['APP_TOKEN']}"}
       query   = { "$where" => "within_circle(incident_location, 47.5951456, -122.331601, 1609.34)"}
       response = HTTParty.get(SEA_CRIME_URI, :headers => headers, :query => query)
+      top_five_events(response)
       data = setup_data(response)
       code = :ok
-    rescue
-      data = {}
-      code = :no_content
-    end
+    # rescue
+    #   data = {}
+    #   code = :no_content
+    # end
     render json: data.as_json, code: code
   end
 
@@ -34,5 +35,19 @@ class ApiController < ApplicationController
         indcident_location_city: event.fetch("incident_location_city", "")
       }
     end
+  end
+
+  def top_five_events(response)
+    hash = Hash.new(0)
+    response.each do |event|
+      group_name = event["event_clearance_group"]
+      hash[group_name] += 1
+    end
+    hash.each_key do |key|
+      response = response.select { |h| h["event_clearance_group"] == key}
+    end
+    binding.pry
+    # response = hash.sort_by { |name, value| -value }.first(5)
+    #hash.max(5) {|a,b| a[1] <=> b[1]}
   end
 end
