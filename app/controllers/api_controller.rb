f9369cff21328ebd15f80d68e5a6248b385a5e02
Loren Wang <wang131.131@gmail.com>
2016-05-16 19:25:33 -0700

    response includes data for only the top 5 events

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -6,14 +6,28 @@ class ApiController < ApplicationController
       headers = { "$$app_token" => "#{ENV['APP_TOKEN']}"}
       query   = { "$where" => "within_circle(incident_location, 47.5951456, -122.331601, 1609.34)"}
       response = HTTParty.get(SEA_CRIME_URI, :headers => headers, :query => query)
-      top_five_events(response)
-      data = setup_data(response)
+      shoot = top_five_events(response)
+      data = setup_data(shoot)
       code = :ok
     # rescue
-    #   data = {}
-    #   code = :no_content
+      # data = {}
+      # code = :no_content
     # end
     render json: data.as_json, code: code
+  end
+
+  def top_five_events(response)
+    hash = Hash.new(0)
+    array = []
+    response.each do |event|
+      group_name = event["event_clearance_group"]
+      hash[group_name] += 1
+    end
+    hash = hash.max(5) {|a,b| a[1] <=> b[1]}.to_h
+    hash.each_key do |key|
+      array << response.select { |h| h["event_clearance_group"] == key}
+    end
+    return array.flatten!
   end
 
   def setup_data(response)
@@ -35,19 +49,5 @@ class ApiController < ApplicationController
         indcident_location_city: event.fetch("incident_location_city", "")
       }
     end
-  end
-
-  def top_five_events(response)
-    hash = Hash.new(0)
-    response.each do |event|
-      group_name = event["event_clearance_group"]
-      hash[group_name] += 1
-    end
-    hash.each_key do |key|
-      response = response.select { |h| h["event_clearance_group"] == key}
-    end
-    binding.pry
-    # response = hash.sort_by { |name, value| -value }.first(5)
-    #hash.max(5) {|a,b| a[1] <=> b[1]}
   end
 end
