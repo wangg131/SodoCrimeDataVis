c6b50700e7eb9fac3c711da090cac737d11bc13b
Loren Wang <wang131.131@gmail.com>
2016-05-16 20:59:50 -0700

    WIP geojson object creation

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -7,13 +7,53 @@ $(document).ready(function() {
   var map = L.mapbox.map('map', 'mapbox.streets')
       .setView([47.5951456, -122.331601], 14);
 
-  $.ajax({
-    dataType: 'text',
-    url: '/',
-    success: function(data) {
-      var geojson;
-      geojson = $.parseJSON(data);
-      return map.featureLayer.setGeoJSON(geojson);
-    }
-  });
+  // var geojson =
+  //   [{
+  //     "type": "FeatureCollection",
+  //     "features": [
+  //       {
+  //         "type": "Feature",
+  //         "geometry": {
+  //           "type": "Point",
+  //           "coordinates": [
+  //             -122.325060336,
+  //             47.597932435
+  //           ]
+  //         },
+  //         "properties": {}
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     "type": "FeatureCollection",
+  //     "features": [
+  //       {
+  //         "type": "Feature",
+  //         "geometry": {
+  //           "type": "Point",
+  //           "coordinates": [
+  //             -122.318435645,
+  //             47.597504516
+  //           ]
+  //         },
+  //         "properties": {}
+  //       }
+  //     ]
+  //   }]
+
+  var myLayer = L.mapbox.featureLayer()
+     .loadURL('/')
+     .addTo(map);
+  myLayer.setGeoJSON(geojson);
+  // var geojson = []
+
+  // $.ajax({
+  //   dataType: 'text',
+  //   url: '/',
+  //   success: function(data) {
+  //     var geojson;
+  //     geojson = $.parseJSON(data);
+  //     return map.featureLayer.setGeoJSON(geojson);
+  //   }
+  // });
 });
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,18 +2,22 @@ require 'httparty'
 
 class ApiController < ApplicationController
   def home
-    begin
+    # begin
       headers = { "$$app_token" => "#{ENV['APP_TOKEN']}"}
       query   = { "$where" => "within_circle(incident_location, 47.5951456, -122.331601, 1609.34)"}
       response = HTTParty.get(SEA_CRIME_URI, :headers => headers, :query => query)
-      response = top_five_events(response)
-      data = fetch_data(response)
-      code = :ok
-    rescue
-      data = {}
-      code = :no_content
+      # response = top_five_events(response)
+      data = geojson(top_five_events(response))
+      # data = top_five_events(response)
+    # rescue
+    #   data = {}
+    # end
+    # raise
+    respond_to do |format|
+      format.html
+      format.json { render json: data.as_json}
     end
-    render json: data.as_json, code: code
+    # render json: data.as_json
   end
 
   private
@@ -29,27 +33,52 @@ class ApiController < ApplicationController
     top_events_hash.each_key do |key|
       top_events_array << response.select { |h| h["event_clearance_group"] == key}
     end
+    # geojson(top_events_array)
     return top_events_array.flatten!
   end
 
-  def fetch_data(response)
-    response.each do |event|
-      response = {
-        event_clearance_code: event.fetch("event_clearance_code", ""),
-        cad_event_number: event.fetch("cad_event_number", ""),
-        event_clearance_subgroup: event.fetch("event_clearance_subgroup", ""),
-        event_clearance_group: event.fetch("event_clearance_group", ""),
-        cad_cdw_id: event.fetch("cad_cdw_id", ""),
-        event_clearance_date: event.fetch("event_clearance_date", ""),
-        district_sector: event.fetch("district_sector", ""),
-        hundred_block_location: event.fetch("hundred_block_location", ""),
-        general_offense_number: event.fetch("general_offense_number", ""),
-        event_clearance_description: event.fetch("event_clearance_description", ""),
-        longitude: event.fetch("longitude", ""),
-        latitude: event.fetch("latitude", ""),
-        census_tract: event.fetch("census_tract", ""),
-        indcident_location_city: event.fetch("incident_location_city", "")
+  def geojson(crime_data)
+    @geojson_object = []
+    crime_data.each do |data|
+      @geojson_object <<
+      {
+        "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                data["longitude"],
+                data["latitude"]
+              ]
+            },
+            "properties": {}
+          }
+        ]
       }
     end
+    return @geojson_object
   end
+
+  # def fetch_data(response)
+  #   response.each do |event|
+  #     response = {
+        # event_clearance_code: event.fetch("event_clearance_code", ""),
+        # cad_event_number: event.fetch("cad_event_number", ""),
+        # event_clearance_subgroup: event.fetch("event_clearance_subgroup", ""),
+        # event_clearance_group: event.fetch("event_clearance_group", ""),
+        # cad_cdw_id: event.fetch("cad_cdw_id", ""),
+        # event_clearance_date: event.fetch("event_clearance_date", ""),
+        # district_sector: event.fetch("district_sector", ""),
+        # hundred_block_location: event.fetch("hundred_block_location", ""),
+        # general_offense_number: event.fetch("general_offense_number", ""),
+        # event_clearance_description: event.fetch("event_clearance_description", ""),
+        # longitude: event.fetch("longitude", ""),
+        # latitude: event.fetch("latitude", ""),
+        # census_tract: event.fetch("census_tract", ""),
+        # indcident_location_city: event.fetch("incident_location_city", "")
+    #   }
+    # end
+  # end
 end
