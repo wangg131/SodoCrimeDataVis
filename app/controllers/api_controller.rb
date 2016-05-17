6df8483b9db02ba4e09a7ea2fc35708e9a4d22ef
Loren Wang <wang131.131@gmail.com>
2016-05-16 19:33:27 -0700

    renamed variables for clarity

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,35 +2,37 @@ require 'httparty'
 
 class ApiController < ApplicationController
   def home
-    # begin
+    begin
       headers = { "$$app_token" => "#{ENV['APP_TOKEN']}"}
       query   = { "$where" => "within_circle(incident_location, 47.5951456, -122.331601, 1609.34)"}
       response = HTTParty.get(SEA_CRIME_URI, :headers => headers, :query => query)
-      shoot = top_five_events(response)
-      data = setup_data(shoot)
+      response = top_five_events(response)
+      data = fetch_data(response)
       code = :ok
-    # rescue
-      # data = {}
-      # code = :no_content
-    # end
+    rescue
+      data = {}
+      code = :no_content
+    end
     render json: data.as_json, code: code
   end
 
+  private
+
   def top_five_events(response)
-    hash = Hash.new(0)
-    array = []
+    top_events_hash = Hash.new(0)
+    top_events_array = []
     response.each do |event|
       group_name = event["event_clearance_group"]
-      hash[group_name] += 1
+      top_events_hash[group_name] += 1
     end
-    hash = hash.max(5) {|a,b| a[1] <=> b[1]}.to_h
-    hash.each_key do |key|
-      array << response.select { |h| h["event_clearance_group"] == key}
+    top_events_hash = top_events_hash.max(5) {|a,b| a[1] <=> b[1]}.to_h
+    top_events_hash.each_key do |key|
+      top_events_array << response.select { |h| h["event_clearance_group"] == key}
     end
-    return array.flatten!
+    return top_events_array.flatten!
   end
 
-  def setup_data(response)
+  def fetch_data(response)
     response.each do |event|
       response = {
         event_clearance_code: event.fetch("event_clearance_code", ""),
