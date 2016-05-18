36a333b0d339ea279d903449e042eceb3c5718c9
Loren Wang <wang131.131@gmail.com>
2016-05-17 21:23:19 -0700

    first test for api.. responded with success

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe ApiController, type: :controller do
+  CRIME_DATA = { cassette_name: "TIMJ_search", record: :new_episodes }
+
+  RSpec.describe ApiController, type: :controller do
+    describe "interacting with the the Seattle crime dataset", vcr: CRIME_DATA do
+      before :each do
+        get :home
+      end
+
+      it "should be successful" do
+        expect(response).to be_ok
+      end
+    end
 
 end
